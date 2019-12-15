49b80b0fa8df8cfc9cd85654b1451fb69da0eb0e
William Fox <wfox96@gmail.com>
2019-12-14 16:54:50 -0800

    add env file

---
+++ env.py
@@ -0,0 +1,26 @@
+from os import environ
+
+MAX_REFRESHES = 5
+
+# region Login Page
+VONS_LOG_IN_LINK = "https://www.vons.com/account/sign-in.html"
+email_text_entry_id = "label-email"
+password_text_entry_id = "label-password"
+sign_in_submit_id = "btnSignIn"
+# endregion
+
+# region Home Page
+sign_in_button_id = "sign-in-profile-text"
+# endregion
+
+# region Just 4 U Page
+VONS_JUST_4_U_Link = "https://www.vons.com/justforu/coupons-deals.html"
+load_more_button_class = "load-more-container"
+add_button_class = "grid-coupon-clip-button"
+add_button_text = "ADD"
+# endregion
+
+# region Authentication
+email = environ.get("email")
+password = environ.get("password")
+# endregion
