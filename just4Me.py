cd554d0b2e8b3cebea35f04090e3a9b7f126a8cf
William Fox <wfox96@gmail.com>
2019-12-15 09:00:10 -0800

    need to push partial changes to work on laptop

--- just4Me.py
+++ just4Me.py
@@ -1,16 +1,16 @@
+import logging.config
 from collections import namedtuple
 from os import getcwd, path
-import logging.config
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, \
-    ElementClickInterceptedException, TimeoutException
+    ElementClickInterceptedException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as e_c
-from selenium.webdriver.support.wait import WebDriverWait
 
 import env
+from websites.basesite import Authentication
+from websites.vons import Vons
 
 TextInput = namedtuple("TextInput", ["id", "keys"])
 
@@ -22,37 +22,14 @@ def main():
     chrome_options = Options()
     # chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(path.join(getcwd(), env.CHROME_DRIVER_DIR, env.CHROME_DRIVER), options=chrome_options)
-    browser = login(browser)
-    wait_for_home_load(browser)
-    browser = just4you(browser)
-    browser.quit()
-
-
-def login(browser: webdriver.Chrome) -> webdriver.Chrome:
-    logger.info("Logging In")
-    text_inputs = [TextInput(env.email_text_entry_id, env.email),
-                   TextInput(env.password_text_entry_id, env.password)]
-
-    browser.get(env.VONS_LOG_IN_LINK)
-
-    for text_input in text_inputs:
-        element = browser.find_element_by_id(text_input.id)
-        element.click()
-        element.send_keys(text_input.keys)
-
-    browser.find_element_by_id(env.sign_in_submit_id).click()
-    logger.info("Logged In")
-    return browser
-
-
-def wait_for_home_load(browser: webdriver.Chrome) -> None:
-    logger.info("Waiting for Home Page to be Loaded")
-    try:
-        WebDriverWait(browser, 3).until(e_c.presence_of_element_located((By.ID, env.sign_in_button_id)))
-        logger.info("Home Page Loaded")
-    except TimeoutException:
-        logger.critical("Home Page Took Too Long to load, quitting")
-        exit(1)
+    site = Vons(browser, authentication=Authentication(env.email, env.password))
+    site.drive()
+    while True:
+        pass
+    # browser = login(browser)
+    # wait_for_home_load(browser)
+    # browser = just4you(browser)
+    # browser.quit()
 
 
 def just4you(browser: webdriver.Chrome) -> webdriver.Chrome:
