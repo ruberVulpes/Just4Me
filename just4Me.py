aa0a26cdcf02f6d2287a1a63d7ef0e2d08f869d5
William Fox <wfox96@gmail.com>
2019-12-23 20:40:36 -0800

    Modularization  (#1)
    * added BaseSite
    
    * renamed self.web_driver to browser for easier autocomplete
    
    * added better defaults to init
    
    * added logging, login, and drive
    
    * fixed init and type hints
    
    * added vons site
    
    * updating logging format
    
    * added wait_for_site_load
    
    * added wait_for_site_load
    
    * need to push partial changes to work on laptop
    
    * chmod +x
    
    * added coupons click
    
    * added total coupons
    
    * added load more function for base site
    
    * fixed continue_button_function
    
    * added proper refresh and added refresh logic to coupons clicked
    
    * removed unneeded code
    
    * removed unneeded input
    
    * added returns to refresh helper for load_more()'s use
    
    * made refresh better
    
    * added vons home url
    
    * removed unneeded env
    
    * updated for new env format
    
    * added timeout default to basesite
    
    * autoformat
    
    * added __get_to_login_page__
    
    * changed logging message
    
    * added a quit if site doesn't load after login
    
    * new env.py format

--- just4Me.py
+++ just4Me.py
@@ -1,16 +1,13 @@
+import logging.config
 from collections import namedtuple
 from os import getcwd, path
-import logging.config
+
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, \
-    ElementClickInterceptedException, TimeoutException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as e_c
-from selenium.webdriver.support.wait import WebDriverWait
 
 import env
+from websites.basesite import Authentication
+from websites.vons import Vons
 
 TextInput = namedtuple("TextInput", ["id", "keys"])
 
@@ -22,82 +19,8 @@ def main():
     chrome_options = Options()
     # chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(path.join(getcwd(), env.CHROME_DRIVER_DIR, env.CHROME_DRIVER), options=chrome_options)
-    browser = login(browser)
-    wait_for_home_load(browser)
-    browser = just4you(browser)
-    browser.quit()
-
-
-def login(browser: webdriver.Chrome) -> webdriver.Chrome:
-    logger.info("Logging In")
-    text_inputs = [TextInput(env.email_text_entry_id, env.email),
-                   TextInput(env.password_text_entry_id, env.password)]
-
-    browser.get(env.VONS_LOG_IN_LINK)
-
-    for text_input in text_inputs:
-        element = browser.find_element_by_id(text_input.id)
-        element.click()
-        element.send_keys(text_input.keys)
-
-    browser.find_element_by_id(env.sign_in_submit_id).click()
-    logger.info("Logged In")
-    return browser
-
-
-def wait_for_home_load(browser: webdriver.Chrome) -> None:
-    logger.info("Waiting for Home Page to be Loaded")
-    try:
-        WebDriverWait(browser, 3).until(e_c.presence_of_element_located((By.ID, env.sign_in_button_id)))
-        logger.info("Home Page Loaded")
-    except TimeoutException:
-        logger.critical("Home Page Took Too Long to load, quitting")
-        exit(1)
-
-
-def just4you(browser: webdriver.Chrome) -> webdriver.Chrome:
-    logger.info("Starting Just4U")
-
-    def is_add_button(button: WebElement) -> bool:
-        return button.text == env.add_button_text and button.is_displayed()
-
-    browser.get(env.VONS_JUST_4_U_Link)
-    try:
-        load_more_button = browser.find_element_by_class_name(env.load_more_button_class)
-        while True:
-            buttons_clicked = 0
-            # Get All Add/Added Buttons on Page
-            add_type_buttons = browser.find_elements_by_class_name(env.add_button_class)
-            # Filter Buttons down to just Add Buttons
-            add_buttons = [button for button in add_type_buttons if is_add_button(button)]
-            for button in add_buttons:
-                button.click()
-                buttons_clicked += 1
-            if load_more_button.is_displayed():
-                load_more_button.click()
-                logger.info(f"Clicking Load More, clicked {buttons_clicked}")
-            else:
-                break
-            load_more_button = browser.find_element_by_class_name(env.load_more_button_class)
-    except NoSuchElementException:
-        logger.info("Finished adding all items")
-        # Finished clicking all the deals
-        pass
-    except StaleElementReferenceException:
-        env.MAX_REFRESHES -= 1
-        if env.MAX_REFRESHES > 0:
-            logger.warning("Stale Button, Refreshing")
-            return just4you(browser)
-        else:
-            logger.error("Stale Button, Max Retries Hit")
-    except ElementClickInterceptedException:
-        env.MAX_REFRESHES -= 1
-        if env.MAX_REFRESHES > 0:
-            logger.warning("Click Intercepted, Refreshing")
-            return just4you(browser)
-        else:
-            logger.error("Click Intercepted, Max Retries Hit")
-    return browser
+    site = Vons(browser, authentication=Authentication(*env.vons_auth))
+    site.drive()
 
 
 if __name__ == '__main__':
---
+++ websites/basesite.py
@@ -0,0 +1,168 @@
+import logging
+from collections import namedtuple
+from typing import Callable, List
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, StaleElementReferenceException, \
+    ElementClickInterceptedException
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+LoginElements = namedtuple("LoginElements", ["email_entry_id", "password_entry_id", "sign_in_submit_id"])
+CouponElement = namedtuple("CouponElements", ["coupon_button_class", "coupon_button_text"])
+Authentication = namedtuple("Authentication", ["email", "password"])
+
+TextInput = namedtuple("TextInput", ["element_id", "keys"])
+
+logger = logging.getLogger(__name__)
+
+
+class BaseSite:
+    site_name: str
+    coupon_program_name: str
+    home_url: str
+    login_url: str
+    coupons_url: str
+
+    login_elements: LoginElements
+    coupon_element: CouponElement
+
+    post_login_element_info: str
+    post_login_element_function: Callable
+
+    continue_button_info: str
+    continue_button_function: Callable
+
+    authentication: Authentication
+
+    browser: webdriver.Chrome
+
+    total_coupons = 0
+    times_load_more = 0
+    load_more_log_frequency = 5
+
+    time_out = 3
+    max_retries = 5
+
+    def __init__(
+            self,
+            browser: webdriver.Chrome,
+            authentication: Authentication,
+    ):
+        self.browser = browser
+        self.authentication = authentication
+
+    def drive(self) -> None:
+        self.login()
+        if not self.wait_for_site_load():
+            return
+        self.go_to_coupon_site()
+        self.click_coupons()
+        while self.load_more():
+            self.click_coupons()
+        logger.info(f"Loaded More {self.times_load_more} times total for Site: {self.site_name}")
+        logger.info(f"Clicked {self.total_coupons} total coupons on Site: {self.site_name}")
+        self.browser.quit()
+
+    def login(self) -> None:
+        logger.info(f"Attempting to log into Site: {self.site_name}")
+        self.__get_to_login_page__()
+        text_inputs = [TextInput(self.login_elements.email_entry_id, self.authentication.email),
+                       TextInput(self.login_elements.password_entry_id, self.authentication.password)]
+        for text_input in text_inputs:
+            element = self.browser.find_element_by_id(text_input.element_id)
+            element.click()
+            element.send_keys(text_input.keys)
+        logger.info(f"Filled user info for Site: {self.site_name}")
+        self.browser.find_element_by_id(self.login_elements.sign_in_submit_id).click()
+        logger.info(f"Successfully Logged into Site: {self.site_name}")
+
+    def __get_to_login_page__(self) -> None:
+        raise NotImplemented
+
+    def wait_for_site_load(self) -> bool:
+        logger.info(f"Waiting for site load for Site: {self.site_name}")
+        try:
+            WebDriverWait(self.browser, self.time_out).until(expected_conditions.presence_of_element_located(
+                (self.post_login_element_function, self.post_login_element_info)))
+        except TimeoutException:
+            logger.critical(f"Post login page took too long to load for Site: {self.site_name}")
+            self.__quit_browser__()
+            return False
+        logger.info(f"Successfully loaded after log in for Site: {self.site_name}")
+        return True
+
+    def go_to_coupon_site(self) -> None:
+        logger.info(f"Going to start program {self.coupon_program_name} on Site: {self.site_name}")
+        self.browser.get(self.coupons_url)
+
+    def click_coupons(self) -> None:
+
+        def get_active_coupon_elements() -> List[WebElement]:
+            def is_active_coupon_element(button: WebElement) -> bool:
+                return button.is_displayed() and button.text == self.coupon_element.coupon_button_text
+
+            coupon_elements = self.browser.find_elements_by_class_name(self.coupon_element.coupon_button_class)
+            return [button for button in coupon_elements if is_active_coupon_element(button)]
+
+        logger.debug(f"Clicking coupons on Site: {self.site_name}")
+        coupons_clicked = 0
+        active_coupon_elements = get_active_coupon_elements()
+        try:
+            for coupon_element in active_coupon_elements:
+                coupon_element.click()
+                coupons_clicked += 1
+        except StaleElementReferenceException:
+            self.__coupon_site_refresh__("StaleElementReference")
+        except ElementClickInterceptedException:
+            self.__coupon_site_refresh__("ElementClickIntercepted")
+        self.total_coupons += coupons_clicked
+        logger.debug(f"Clicked {coupons_clicked} coupons on Site: {self.site_name}")
+
+    def load_more(self) -> bool:
+        logger.debug(f"Attempting to load more on Site: {self.site_name}")
+        try:
+            load_element: WebElement = self.continue_button_function(self.continue_button_info)
+            if load_element.is_displayed():
+                load_element.click()
+                logger.debug(f"Successfully loaded more on Site: {self.site_name}")
+                self.times_load_more += 1
+                self.__load_more_logger_helper__()
+            else:
+                logger.error(f"Load more button not displayed on Site: {self.site_name}")
+            return True
+        except NoSuchElementException:
+            logger.warning(f"Failed to load more on Site: {self.site_name}")
+            return False
+        except AttributeError:
+            necessary_attributes = ["continue_button_function", "continue_button_info"]
+            logger.warning(f"Missing attributes needed  {' or '.join(necessary_attributes)} for Site: {self.site_name}")
+            return False
+        except StaleElementReferenceException:
+            return self.__coupon_site_refresh__("StaleElementReference")
+        except ElementClickInterceptedException:
+            return self.__coupon_site_refresh__("ElementClickIntercepted")
+
+    def __coupon_site_refresh__(self, reason: str) -> bool:
+
+        def base_msg() -> str:
+            return f"{self.coupons_url} for Site: {self.site_name} due to {reason}"
+
+        self.max_retries -= 1
+        if self.max_retries > 0:
+            logger.warning(f"Refreshing {base_msg()}")
+            self.browser.get(self.home_url)
+            self.browser.get(self.coupons_url)
+            return True
+        else:
+            logger.error(f"Max Retries hit for url: {base_msg()}")
+            self.__quit_browser__()
+            return False
+
+    def __load_more_logger_helper__(self) -> None:
+        if self.times_load_more % self.load_more_log_frequency == 0:
+            logger.info(f"Loaded more {self.times_load_more} times for Site: {self.site_name}")
+
+    def __quit_browser__(self) -> None:
+        logger.info("Quitting Browser")
+        self.browser.quit()
