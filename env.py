5f25a1bf5feb78caa3bb0b7623f8362daf450414
William Fox <wfox96@gmail.com>
2020-07-05 18:40:51 -0700

    using webdriver handler

--- env.py
+++ env.py
@@ -1,16 +1,7 @@
-from os import environ
-
-# region Environment
-CHROME_DRIVER = "win_chromedriver" if environ.get("env") == "windows" else "mac_chromedriver"
-CHROME_DRIVER_DIR = "chromedriver"
-# endregion
-
-# region Authentication
+import os
 
 # region Vons
-__vons_email__ = environ.get("vons_email")
-__vons_password__ = environ.get("vons_password")
+__vons_email__ = os.environ.get("vons_email")
+__vons_password__ = os.environ.get("vons_password")
 vons_auth = (__vons_email__, __vons_password__)
-# endregion
-
 # endregion
