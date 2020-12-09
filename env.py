2c011d268570fcecf904c4d46f32d044d9c36a72
William Fox <wfox96@gmail.com>
2020-12-08 17:37:57 -0800

    added support for Albertsons

--- env.py
+++ env.py
@@ -5,3 +5,9 @@ __vons_email__ = os.environ.get("vons_email")
 __vons_password__ = os.environ.get("vons_password")
 vons_auth = (__vons_email__, __vons_password__)
 # endregion
+
+
+# region Albertsons
+# They're the same authentication
+albertsons_auth = (__vons_email__, __vons_password__)
+# endregion
---
+++ websites/albertsons.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from websites.basesite import BaseSite, Authentication, LoginElements, CouponElement
+
+
+class Albertsons(BaseSite):
+    site_name = "Albertsons"
+    coupon_program_name = "just4U"
+    home_url = "https://www.albertsons.com/"
+    login_url = "https://www.albertsons.com/account/sign-in.html"
+    login_elements = LoginElements("label-email", "label-password", "btnSignIn")
+
+    coupon_element = CouponElement("grid-coupon-clip-button", "Clip Coupon")
+    coupons_url = "https://www.albertsons.com/justforu/coupons-deals.html"
+    continue_button_info = "load-more-container"
+
+    post_login_element_info = "sign-in-profile-text"
+    post_login_element_function = By.ID
+
+    def __init__(
+            self,
+            browser: webdriver.Chrome,
+            authentication: Authentication,
+    ):
+        self.continue_button_function = browser.find_element_by_class_name
+        super().__init__(browser, authentication)
+
+    def __get_to_login_page__(self) -> None:
+        self.browser.get(self.login_url)
