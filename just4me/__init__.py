68c01848ea61e417d10ca22330e96d4bdb133609
William Fox <wfox96@gmail.com>
2020-12-20 11:40:48 -0800

    added favicon

--- just4me/__init__.py
+++ just4me/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 import env
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='')
 app.config['SECRET_KEY'] = env.secret_key
 
 logging.config.fileConfig(fname='logger.ini', disable_existing_loggers=False)
