2f6e2d7fbe5cf0d7aba29b119ad932513718359c
William Fox <wfox96@gmail.com>
2020-12-19 19:19:02 -0800

    added vons/albertsons

--- just4me/routes.py
+++ just4me/routes.py
@@ -1,8 +1,28 @@
+from threading import Thread
+
+import env
 from just4me import app
+from just4me import just4me
+from just4me.websites import Authentication
 
 
 @app.route(rule='/')
 @app.route(rule='/home')
-@app.route(rule='/')
 def home():
     return 'Hello World'
+
+
+@app.route(rule='/vons')
+@app.route(rule='/websites/vons')
+def vons():
+    authentication = Authentication(*env.vons_auth)
+    Thread(target=just4me.vons, args=(authentication,)).start()
+    return 'Running Vons Coupons with env.py'
+
+
+@app.route(rule='/albertsons')
+@app.route(rule='/websites/albertsons')
+def albertsons():
+    authentication = Authentication(*env.vons_auth)
+    Thread(target=just4me.albertsons, args=(authentication,)).start()
+    return 'Running Albertsons Coupons with env.py'
