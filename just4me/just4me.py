32736d5cce86f70a2456b3e6587831f18af7a59e
William Fox <wfox96@gmail.com>
2020-12-19 22:35:12 -0800

    heroku selenium

--- just4me/just4me.py
+++ just4me/just4me.py
@@ -9,6 +9,13 @@ from just4me.websites.albertsons import Albertsons
 from just4me.websites.basesite import BaseSite
 from just4me.websites.vons import Vons
 
+GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.binary_location = GOOGLE_CHROME_PATH
+
 
 def albertsons(authentication: Authentication):
     _base_site(authentication, Albertsons)
@@ -19,7 +26,7 @@ def vons(authentication: Authentication):
 
 
 def _base_site(authentication: Authentication, site: Type[BaseSite]):
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     site(browser, authentication=authentication).drive()
