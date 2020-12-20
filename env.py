4545cb93ecc33ef49923bddca1ca38f63f6062de
William Fox <wfox96@gmail.com>
2020-12-20 10:39:11 -0800

    removing unused env vars

--- env.py
+++ env.py
@@ -6,15 +6,3 @@ chrome_path = os.environ['GOOGLE_CHROME_BIN']
 # region Flask
 secret_key = os.environ['SECRET_KEY']
 # endregion
-
-# region Vons
-__vons_email__ = os.environ['vons_email']
-__vons_password__ = os.environ['vons_password']
-vons_auth = (__vons_email__, __vons_password__)
-# endregion
-
-
-# region Albertsons
-# They're the same authentication
-albertsons_auth = (__vons_email__, __vons_password__)
-# endregion
