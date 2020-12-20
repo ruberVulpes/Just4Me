77db6601879e637b576744196a99d0e5b425b7ea
William Fox <wfox96@gmail.com>
2020-12-20 10:30:16 -0800

    heroku selenium

--- just4me/just4me.py
+++ just4me/just4me.py
@@ -13,6 +13,7 @@ GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
+chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
