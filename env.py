9e2dbf1f10372addc7037aae3ac1755f8a129f84
William Fox <wfox96@gmail.com>
2020-12-20 11:00:11 -0800

    env++

--- env.py
+++ env.py
@@ -1,13 +1,17 @@
 import os
 
+env = os.environ.get('ENV', 'dev')
+is_prod = env.lower() == 'prod'
+
 # region Selenium on Heroku
 chromedriver_path = os.environ.get('CHROMEDRIVER_PATH')
 chromedriver_bin = os.environ.get('GOOGLE_CHROME_BIN')
+if is_prod:
+    assert chromedriver_path is not None
+    assert chromedriver_bin is not None
 # endregion
 
 token = os.environ['TOKEN']
-env = os.environ.get('ENV', 'dev')
-is_prod = env.lower() == 'prod'
 
 # region Flask
 secret_key = os.environ['SECRET_KEY']
