38ae22e50c1277addaa164998812476572e1869b
William Fox <wfox96@gmail.com>
2021-02-13 10:30:00 -0800

    added SO post in comments

--- env.py
+++ env.py
@@ -4,6 +4,7 @@ env = os.environ.get('ENV', 'dev')
 is_prod = env.lower() == 'prod'
 
 # region Selenium on Heroku
+# https://stackoverflow.com/questions/48723397/how-to-set-path-to-chromedriver-in-heroku-chromedriver-buildpack/56648878
 chromedriver_path = os.environ.get('CHROMEDRIVER_PATH')
 chromedriver_bin = os.environ.get('GOOGLE_CHROME_BIN')
 if is_prod:
