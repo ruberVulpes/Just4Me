1ee40d927618f8e14328e40a98ec4fe7263071d6
William Fox <wfox96@gmail.com>
2021-02-25 16:57:12 -0800

    refactor

--- just4me/websites/albertsons.py
+++ just4me/websites/albertsons.py
@@ -1,31 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.remote import webdriver
 
-from just4me.websites import LoginElements, CouponElement, Authentication
-from just4me.websites.basesite import BaseSite
+from just4me.websites import UserPass
+from just4me.websites.vons import Vons
 
 
-class Albertsons(BaseSite):
+# Albertson's site is the same as Vons but just different urls
+class Albertsons(Vons):
     site_name = "Albertsons"
     coupon_program_name = "just4U"
-    home_url = "https://www.albertsons.com/"
-    login_url = "https://www.albertsons.com/account/sign-in.html"
-    login_elements = LoginElements("label-email", "label-password", "btnSignIn")
 
-    coupon_element = CouponElement("grid-coupon-clip-button", "Clip Coupon")
-    coupons_url = "https://www.albertsons.com/justforu/coupons-deals.html"
-    continue_button_info = "load-more-container"
+    home_url = 'https://www.albertsons.com/'
+    login_url = 'https://www.albertsons.com/account/sign-in.html'
+    coupons_url = 'https://www.albertsons.com/justforu/coupons-deals.html'
 
-    post_login_element_info = "sign-in-profile-text"
-    post_login_element_function = By.ID
-
-    def __init__(
-            self,
-            browser: webdriver.Chrome,
-            authentication: Authentication,
-    ):
-        self.continue_button_function = browser.find_element_by_class_name
-        super().__init__(browser, authentication)
-
-    def __get_to_login_page__(self) -> None:
-        self.browser.get(self.login_url)
+    def __init__(self, browser: webdriver.WebDriver, user_pass: UserPass):
+        super().__init__(browser, user_pass)
