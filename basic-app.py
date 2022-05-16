8b9cef63e0eb43b80bbfa3ede0ef4544f28f9b1c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-05-16 14:37:03 +0800

    Delete basic-app.py

--- basic-app.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from just4me.just4me import vons, albertsons
-from just4me.websites import UserPass
-
-if __name__ == '__main__':
-    albertsons(UserPass(os.getenv('email'), os.getenv('password')))
